4dd50e8272cc385cbeba522c9b389f99ae3bf443
Oshan12345 <bsagor99@gmail.com>
2021-05-20 13:15:10 +0600

    new project add

--- pages/projects.js
+++ pages/projects.js
@@ -17,6 +17,20 @@ const allProjects = [
     ],
   },
   {
+    name: "Buddy-zone",
+    image: "/buddy-zone.png",
+    url: "https://buddy-zone.vercel.app/",
+    clintCode: "https://github.com/sagar-biswas1/buddy-zone-client-side",
+    serverCode: "https://github.com/sagar-biswas1/buddy-zone-server-side",
+    title: ` A full-stack social media app`,
+    points: [
+      "A user can create his/her account. On creation s/he will get a welcome message though his/her email. ",
+      "In case one forgets his password s/he can also change his password after getting a confirmation link through email (sendgrid is integrated). ",
+      "Users can publish their posts. They can also delete their posts. There is a search functionality for searching the users by name.",
+      "In case one User follow others s/he can chat also chat with them in real time(Pusher is integrated here.)",
+    ],
+  },
+  {
     name: "Book's Voice",
     image: "/bookVoice.png",
     url: "https://book-voice.netlify.app/",
